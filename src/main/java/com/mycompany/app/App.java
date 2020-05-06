dfb1974318d103dfbc380da46805ed1c58c490db
chenzhanghong <chenzhanghong@gmail.com>
2020-05-06 14:30:29 +0800

    '.'

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -3,19 +3,20 @@ package com.mycompany.app;
 /**
  * Hello world!
  */
-public class App
-{
+public class App {
 
-    private final String message = "Hello World!";
+  private final String message = "Hello World!";
+  private final String hello = "hello";
 
-    public App() {}
+  public App() {
+  }
 
-    public static void main(String[] args) {
-        System.out.println(new App().getMessage());
-    }
+  public static void main(String[] args) {
+    System.out.println(new App().getMessage());
+  }
 
-    private final String getMessage() {
-        return message;
-    }
+  private final String getMessage() {
+    return message;
+  }
 
 }
